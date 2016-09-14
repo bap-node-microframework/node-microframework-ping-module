6dfa9732b7fc19d1c0d20b74edc7d1bf29141e38
Nicola RIZZO <nicola.n.rizzo@gmail.com>
2016-09-14 11:07:16 +0200

    Fix container bug

--- dist/app/PingModule.js
+++ dist/app/PingModule.js
@@ -6,10 +6,13 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var PingController_1 = require("./PingController");
 var core_1 = require('bap-node-microframework/core');
+var core_2 = require('bap-node-microframework/core');
 var PingModule = (function (_super) {
     __extends(PingModule, _super);
-    function PingModule() {
-        _super.apply(this, arguments);
+    function PingModule(app, io, container) {
+        container.registerService('router', container.get("router"));
+        core_2.Container.setApplicationInstance(container);
+        _super.call(this, app, io);
     }
     PingModule.prototype.registerControllers = function () {
         this.app.use(PingController_1.default);
--- src/app/PingModule.ts
+++ src/app/PingModule.ts
@@ -1,7 +1,14 @@
 import { default as PingRouter } from "./PingController";
 import { Module } from 'bap-node-microframework/core';
+import { Container } from 'bap-node-microframework/core';
 
 export class PingModule extends Module {
+    constructor(app, io, container) {
+        container.registerService('router', container.get("router"));
+        Container.setApplicationInstance(container);
+        super(app, io);
+    }
+
     registerControllers() {
         this.app.use(PingRouter);
     }
